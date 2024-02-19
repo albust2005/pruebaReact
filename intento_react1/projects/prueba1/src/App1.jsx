544c1fb4aa57b4d651fbafe1dcf192b8b9a1731d
albust2005 <brothermas64@gmail.com>
2024-02-18 19:52:23 -0500

    Es el proyecto desarrollado hasta el momento con React

---
+++ intento_react1/projects/prueba1/src/App1.jsx
@@ -0,0 +1,58 @@
+import { useState } from 'react'
+import React from 'react'
+import './App.css' 
+import { TwitterFollowCard } from './TwitterFollowCard.jsx'
+
+const users = [
+    {
+        userName: 'midudev',
+        name: 'Miguel Ángel Durán',
+        isFollowing: true
+    },
+    {
+        userName: 'pherald',
+        name: 'Pablo H.',
+        isFollowing: false
+    },
+    {
+        userName: 'PacoHdezs',
+        name: 'Paco Hdez',
+        isFollowing: true
+    },
+    {
+        userName: 'TMChein',
+        name: 'Tomas',
+        isFollowing: false
+    }
+]
+
+export function App () {
+    // const formatUserName = (userName) => `@${userName}`
+    // const [name, setName] = useState('midudev')
+
+    return (
+        <section className='App'>
+            {
+                users.map(user => {
+                    const { userName, name, isFollowing } = user
+                    return(
+                        <TwitterFollowCard 
+                            key={userName}
+                            userName={userName}
+                            initialIsFollowing={isFollowing}
+                        >
+                            {name}
+                        </TwitterFollowCard>
+                    )
+                })
+            }
+
+            {/* <TwitterFollowCard userName='midudev'> 
+                Miguel Ángel Durán
+            </TwitterFollowCard>
+            <TwitterFollowCard userName="pherald"> 
+                Pablo Hernandez
+            </TwitterFollowCard> */}
+        </section>
+    )
+}
